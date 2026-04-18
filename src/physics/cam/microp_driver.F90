09159c1a4b07f3ede71c71364c7eb272c655fefe
Ruitong <cnitlrt@gmail.com>
2026-04-17 18:52:19 -0600

    cam: port microp_driver to Codon

--- src/physics/cam/microp_driver.F90
+++ src/physics/cam/microp_driver.F90
@@ -19,6 +19,7 @@ use micro_mg_cam,   only: micro_mg_cam_readnl, micro_mg_cam_register, &
 use cam_logfile,    only: iulog
 use cam_abortutils, only: endrun
 use perf_mod,       only: t_startf, t_stopf
+use spmd_utils,     only: masterproc
 
 implicit none
 private
@@ -33,6 +34,10 @@ public :: &
    microp_driver_tend
 
 character(len=16)  :: microp_scheme   ! Microphysics scheme
+logical :: use_native_impl = .false.
+logical :: impl_selected = .false.
+integer :: codon_scheme_code = 0
+logical :: codon_scheme_selected = .false.
 
 !===============================================================================
 contains
@@ -165,17 +170,85 @@ subroutine microp_driver_tend(state, ptend, dtime, pbuf)
 
    real(r8), intent(in)  :: dtime                    ! Timestep
 
-   ! Local variables
-
-   integer :: lchnk
-   integer :: ncol
-
    !======================================================================
 
-   lchnk = state%lchnk
-   ncol  = state%ncol
+   call microp_driver_select_impl()
+
+   if (use_native_impl) then
+      call microp_driver_tend_native(state, ptend, dtime, pbuf)
+      return
+   end if
+
+   call microp_driver_select_codon_scheme()
 
    ! Call MG Microphysics
+
+   select case (codon_scheme_code)
+   case (1)
+      call t_startf('microp_mg_tend')
+      call micro_mg_cam_tend(state, ptend, dtime, pbuf)
+      call t_stopf('microp_mg_tend')
+   case (2)
+      ! microp_driver doesn't handle this one
+      continue
+   case default
+      call microp_driver_tend_native(state, ptend, dtime, pbuf)
+   end select
+
+end subroutine microp_driver_tend
+
+!===============================================================================
+
+subroutine microp_driver_select_codon_scheme()
+
+   use iso_c_binding, only: c_int64_t, c_loc, c_ptr
+
+   integer :: i
+   integer(c_int64_t), target :: scheme_ascii(len(microp_scheme))
+   integer(c_int64_t), target :: scheme_code
+   integer(c_int64_t), target :: status_code
+
+   interface
+      subroutine microp_driver_select_scheme_codon(scheme_len_c, scheme_ascii_p, scheme_code_p, status_p) &
+           bind(c, name="microp_driver_select_scheme_codon")
+         use iso_c_binding, only: c_int64_t, c_ptr
+         integer(c_int64_t), value :: scheme_len_c
+         type(c_ptr), value :: scheme_ascii_p, scheme_code_p, status_p
+      end subroutine microp_driver_select_scheme_codon
+   end interface
+
+   if (codon_scheme_selected) return
+
+   do i = 1, len(microp_scheme)
+      scheme_ascii(i) = int(iachar(microp_scheme(i:i)), c_int64_t)
+   end do
+
+   scheme_code = 0_c_int64_t
+   status_code = 0_c_int64_t
+   call microp_driver_select_scheme_codon( &
+        int(len(microp_scheme), c_int64_t), c_loc(scheme_ascii(1)), c_loc(scheme_code), c_loc(status_code) &
+   )
+
+   if (status_code /= 0_c_int64_t) then
+      codon_scheme_code = -1
+   else
+      codon_scheme_code = int(scheme_code)
+   end if
+
+   codon_scheme_selected = .true.
+
+end subroutine microp_driver_select_codon_scheme
+
+!===============================================================================
+
+subroutine microp_driver_tend_native(state, ptend, dtime, pbuf)
+
+   ! Call the native microphysics parameterization run methods.
+
+   type(physics_state), intent(in)    :: state
+   type(physics_ptend), intent(out)   :: ptend
+   type(physics_buffer_desc), pointer :: pbuf(:)
+   real(r8), intent(in)               :: dtime
 
    select case (microp_scheme)
    case ('MG')
@@ -189,6 +262,43 @@ subroutine microp_driver_tend(state, ptend, dtime, pbuf)
       call endrun('microp_driver_tend:: unrecognized microp_scheme')
    end select
 
-end subroutine microp_driver_tend
+end subroutine microp_driver_tend_native
+
+!===============================================================================
+
+subroutine microp_driver_select_impl()
+
+   character(len=32) :: impl_name
+   integer :: status, n, i, code
+
+   if (impl_selected) return
+
+   impl_name = 'codon'
+   call get_environment_variable('MICROP_DRIVER_IMPL', value=impl_name, length=n, status=status)
+
+   if (status == 0 .and. n > 0) then
+      do i = 1, n
+         code = iachar(impl_name(i:i))
+         if (code >= iachar('A') .and. code <= iachar('Z')) then
+            impl_name(i:i) = achar(code + iachar('a') - iachar('A'))
+         end if
+      end do
+      use_native_impl = trim(adjustl(impl_name(:n))) == 'native'
+   else
+      use_native_impl = .false.
+   end if
+
+   impl_selected = .true.
+
+   if (masterproc) then
+      if (use_native_impl) then
+         write(iulog,*) 'microp_driver_tend implementation = native'
+      else
+         write(iulog,*) 'microp_driver_tend implementation = codon'
+      end if
+      call flush(iulog)
+   end if
+
+end subroutine microp_driver_select_impl
 
 end module microp_driver
---
+++ src/physics/cam/microp_driver_codon.py
@@ -0,0 +1,36 @@
+@export
+def microp_driver_select_scheme_codon(
+    scheme_len: int,
+    scheme_ascii_p: cobj,
+    scheme_code_p: cobj,
+    status_p: cobj,
+):
+    scheme_ascii = Ptr[int](scheme_ascii_p)
+    scheme_code = Ptr[int](scheme_code_p)
+    status = Ptr[int](status_p)
+
+    status[0] = 0
+    scheme_code[0] = 0
+
+    n = scheme_len
+    while n > 0 and scheme_ascii[n - 1] == 32:
+        n -= 1
+
+    if n == 2:
+        c1 = scheme_ascii[0]
+        c2 = scheme_ascii[1]
+
+        if c1 >= 65 and c1 <= 90:
+            c1 += 32
+        if c2 >= 65 and c2 <= 90:
+            c2 += 32
+
+        if c1 == 109 and c2 == 103:
+            scheme_code[0] = 1
+            return
+
+        if c1 == 114 and c2 == 107:
+            scheme_code[0] = 2
+            return
+
+    status[0] = 1
