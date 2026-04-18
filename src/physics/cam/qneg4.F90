515a99fec60c5c91dabaacb7d63e31758a8f58de
Ruitong <cnitlrt@gmail.com>
2026-04-17 18:52:16 -0600

    cam: port qneg4 to Codon

--- src/physics/cam/qneg4.F90
+++ src/physics/cam/qneg4.F90
@@ -24,6 +24,177 @@ subroutine qneg4 (subnam  ,lchnk   ,ncol    ,ztodt   ,        &
    use physconst,    only: gravit, latvap
    use constituents, only: qmin, pcnst
    use cam_logfile,  only: iulog
+   use spmd_utils,   only: masterproc
+
+   !water isotopes:
+   use water_types,       only: iwtvap
+   use water_tracer_vars, only: trace_water, wisotope, WTRC_MAX_CNST, wtrc_iatype, wtrc_ntype, iwspec, wtrc_qmin, wtrc_fixed_rstd
+   use water_tracers,     only: wtrc_get_rstd
+   use water_isotopes,    only: pwtspec
+   use iso_c_binding,     only: c_double, c_int64_t, c_loc, c_ptr
+
+   implicit none
+
+!
+! Input arguments
+!
+   character*8, intent(in) :: subnam         ! name of calling routine
+!
+   integer, intent(in) :: lchnk              ! chunk index
+   integer, intent(in) :: ncol               ! number of atmospheric columns
+!
+   real(r8), intent(in) :: ztodt             ! two times model timestep (2 delta-t)
+   real(r8), target, intent(in) :: qbot(pcols,pcnst) ! moisture at lowest model level
+   real(r8), target, intent(in) :: srfrpdel(pcols)   ! 1./(pint(K+1)-pint(K))
+!
+! Input/Output arguments
+!
+   real(r8), target, intent(inout) :: shflx(pcols)   ! Surface sensible heat flux (J/m2/s)
+   real(r8), target, intent(inout) :: lhflx(pcols)   ! Surface latent   heat flux (J/m2/s)
+   real(r8), target, intent(inout) :: qflx (pcols,pcnst)   ! surface water flux (kg/m^2/s)
+
+   logical, save :: use_native_impl = .false.
+   logical, save :: impl_selected = .false.
+
+   integer :: ivap
+   integer :: m
+   integer(c_int64_t), target :: indxexc(pcols)    ! index array of points with excess flux
+   integer(c_int64_t), target :: nptsexc           ! number of points with excess flux
+   integer(c_int64_t), target :: iw                ! i index of worst violator
+   integer(c_int64_t), target :: wtrc_iatype_vap(WTRC_MAX_CNST)
+   integer(c_int64_t), target :: iwspec64(pcnst)
+   real(c_double), target :: worst                 ! biggest violator
+   real(c_double), target :: excess(pcols)         ! Excess downward sfc latent heat flux
+   real(c_double), target :: qfxo(pcols,pcnst)     ! initial tracer flux
+   real(c_double), target :: rstd(pwtspec)
+
+   interface
+      subroutine qneg4_codon(ncol_c, pcols_c, pcnst_c, trace_water_on_c, iwtvap_n_c, &
+           qmin1_c, ztodt_c, gravit_c, latvap_c, wtrc_qmin_c, &
+           qbot_p, srfrpdel_p, shflx_p, lhflx_p, qflx_p, indxexc_p, excess_p, qfxo_p, &
+           nptsexc_p, worst_p, iw_p, wtrc_iatype_vap_p, iwspec_p, rstd_p) bind(c, name="qneg4_codon")
+         use iso_c_binding, only: c_double, c_int64_t, c_ptr
+         integer(c_int64_t), value :: ncol_c, pcols_c, pcnst_c, trace_water_on_c, iwtvap_n_c
+         real(c_double), value :: qmin1_c, ztodt_c, gravit_c, latvap_c, wtrc_qmin_c
+         type(c_ptr), value :: qbot_p, srfrpdel_p, shflx_p, lhflx_p, qflx_p, indxexc_p, excess_p, qfxo_p
+         type(c_ptr), value :: nptsexc_p, worst_p, iw_p, wtrc_iatype_vap_p, iwspec_p, rstd_p
+      end subroutine qneg4_codon
+   end interface
+
+!
+!-----------------------------------------------------------------------
+!
+
+   call qneg4_select_impl()
+
+   if (use_native_impl) then
+      call qneg4_native(subnam, lchnk, ncol, ztodt, qbot, srfrpdel, shflx, lhflx, qflx)
+      return
+   end if
+
+   if (trace_water) then
+      do ivap = 1, WTRC_MAX_CNST
+         wtrc_iatype_vap(ivap) = 0_c_int64_t
+      end do
+      do ivap = 1, wtrc_ntype(iwtvap)
+         wtrc_iatype_vap(ivap) = int(wtrc_iatype(ivap, iwtvap), c_int64_t)
+      end do
+      do m = 1, pcnst
+         iwspec64(m) = int(iwspec(m), c_int64_t)
+      end do
+      do m = 1, pwtspec
+         rstd(m) = real(wtrc_get_rstd(m), c_double)
+      end do
+   else
+      wtrc_iatype_vap = 0_c_int64_t
+      iwspec64 = 0_c_int64_t
+      rstd = 0._c_double
+   end if
+
+   call qneg4_codon( &
+        int(ncol, c_int64_t), int(pcols, c_int64_t), int(pcnst, c_int64_t), &
+        merge(1_c_int64_t, 0_c_int64_t, trace_water), int(wtrc_ntype(iwtvap), c_int64_t), &
+        real(qmin(1), c_double), real(ztodt, c_double), real(gravit, c_double), real(latvap, c_double), real(wtrc_qmin, c_double), &
+        c_loc(qbot), c_loc(srfrpdel), c_loc(shflx), c_loc(lhflx), c_loc(qflx), c_loc(indxexc), c_loc(excess), c_loc(qfxo), &
+        c_loc(nptsexc), c_loc(worst), c_loc(iw), c_loc(wtrc_iatype_vap), c_loc(iwspec64), c_loc(rstd) &
+   )
+
+   if (nptsexc > 10_c_int64_t) then
+      write(iulog,9000) subnam, int(nptsexc), real(worst, r8), lchnk, int(iw), get_lat_p(lchnk, int(iw)), get_lon_p(lchnk, int(iw))
+   end if
+
+   return
+9000 format(' QNEG4 WARNING from ',a8 &
+            ,' Max possible LH flx exceeded at ',i6,' points. ' &
+            ,', Worst excess = ',1pe12.4 &
+            ,', lchnk = ',i6 &
+            ,', i = ',i6 &
+            ,', same as indices lat =', i6 &
+            ,', lon =', i6 &
+           )
+
+contains
+
+   subroutine qneg4_select_impl()
+      character(len=32) :: impl_name
+      integer :: status, n, i, code
+
+      if (impl_selected) return
+
+      impl_name = 'codon'
+      call get_environment_variable('QNEG4_IMPL', value=impl_name, length=n, status=status)
+
+      if (status == 0 .and. n > 0) then
+         do i = 1, n
+            code = iachar(impl_name(i:i))
+            if (code >= iachar('A') .and. code <= iachar('Z')) then
+               impl_name(i:i) = achar(code + iachar('a') - iachar('A'))
+            end if
+         end do
+         use_native_impl = trim(adjustl(impl_name(:n))) == 'native'
+      else
+         use_native_impl = .false.
+      end if
+
+      impl_selected = .true.
+
+      if (masterproc) then
+         if (use_native_impl) then
+            write(iulog,*) 'qneg4 implementation = native'
+         else
+            write(iulog,*) 'qneg4 implementation = codon'
+         end if
+         call flush(iulog)
+      end if
+   end subroutine qneg4_select_impl
+
+end subroutine qneg4
+
+subroutine qneg4_native (subnam  ,lchnk   ,ncol    ,ztodt   ,        &
+                         qbot    ,srfrpdel,shflx   ,lhflx   ,qflx    )
+!----------------------------------------------------------------------- 
+! 
+! Purpose: 
+! Check if moisture flux into the ground is exceeding the total
+! moisture content of the lowest model layer (creating negative moisture
+! values).  If so, then subtract the excess from the moisture and
+! latent heat fluxes and add it to the sensible heat flux.
+! 
+! Method: 
+! <Describe the algorithm(s) used in the routine.> 
+! <Also include any applicable external references.> 
+! 
+! Author: J. Olson
+! 
+! Water isotopes added by J. Nusbaumer - Mar 2011
+!
+!-----------------------------------------------------------------------
+   use shr_kind_mod, only: r8 => shr_kind_r8
+   use ppgrid
+   use phys_grid,    only: get_lat_p, get_lon_p
+   use physconst,    only: gravit, latvap
+   use constituents, only: qmin, pcnst
+   use cam_logfile,  only: iulog
 
    !water isotopes:
    use water_types,   only: iwtvap
@@ -139,4 +310,4 @@ subroutine qneg4 (subnam  ,lchnk   ,ncol    ,ztodt   ,        &
             ,', same as indices lat =', i6 &
             ,', lon =', i6 &
            )
-end subroutine qneg4
+end subroutine qneg4_native
---
+++ src/physics/cam/qneg4_codon.py
@@ -0,0 +1,108 @@
+@inline
+def _col_idx(icol: int) -> int:
+    """shflx/lhflx/srfrpdel/excess/indxexc declared as (pcols)"""
+    return icol - 1
+
+
+@inline
+def _q_idx(icol: int, mconst: int, pcols: int) -> int:
+    """qbot/qflx/qfxo declared as (pcols, pcnst)"""
+    return (icol - 1) + (mconst - 1) * pcols
+
+
+@inline
+def _spec_idx(ispec: int) -> int:
+    """rstd declared as (pwtspec)"""
+    return ispec - 1
+
+
+@inline
+def _wtrc_ratio(ispec: int, qtrc: float, qtot: float, wtrc_qmin: float, rstd) -> float:
+    if abs(qtot) < wtrc_qmin:
+        return rstd[_spec_idx(ispec)]
+    return qtrc / qtot
+
+
+@export
+def qneg4_codon(
+    ncol: int,
+    pcols: int,
+    pcnst: int,
+    trace_water_on: int,
+    iwtvap_n: int,
+    qmin1: float,
+    ztodt: float,
+    gravit: float,
+    latvap: float,
+    wtrc_qmin: float,
+    qbot_p: cobj,
+    srfrpdel_p: cobj,
+    shflx_p: cobj,
+    lhflx_p: cobj,
+    qflx_p: cobj,
+    indxexc_p: cobj,
+    excess_p: cobj,
+    qfxo_p: cobj,
+    nptsexc_p: cobj,
+    worst_p: cobj,
+    iw_p: cobj,
+    wtrc_iatype_vap_p: cobj,
+    iwspec_p: cobj,
+    rstd_p: cobj,
+):
+    qbot = Ptr[float](qbot_p)
+    srfrpdel = Ptr[float](srfrpdel_p)
+    shflx = Ptr[float](shflx_p)
+    lhflx = Ptr[float](lhflx_p)
+    qflx = Ptr[float](qflx_p)
+    indxexc = Ptr[int](indxexc_p)
+    excess = Ptr[float](excess_p)
+    qfxo = Ptr[float](qfxo_p)
+    nptsexc = Ptr[int](nptsexc_p)
+    worst = Ptr[float](worst_p)
+    iw = Ptr[int](iw_p)
+    wtrc_iatype_vap = Ptr[int](wtrc_iatype_vap_p)
+    iwspec = Ptr[int](iwspec_p)
+    rstd = Ptr[float](rstd_p)
+
+    for m in range(1, pcnst + 1):
+        for i in range(1, ncol + 1):
+            qfxo[_q_idx(i, m, pcols)] = qflx[_q_idx(i, m, pcols)]
+
+    nptsexc[0] = 0
+
+    for i in range(1, ncol + 1):
+        excess[_col_idx(i)] = qflx[_q_idx(i, 1, pcols)] - (
+            qmin1 - qbot[_q_idx(i, 1, pcols)]
+        ) / (ztodt * gravit * srfrpdel[_col_idx(i)])
+
+        if excess[_col_idx(i)] < 0.0:
+            nptsexc[0] += 1
+            indxexc[_col_idx(nptsexc[0])] = i
+            qflx[_q_idx(i, 1, pcols)] = qflx[_q_idx(i, 1, pcols)] - excess[_col_idx(i)]
+            lhflx[_col_idx(i)] = lhflx[_col_idx(i)] - excess[_col_idx(i)] * latvap
+            shflx[_col_idx(i)] = shflx[_col_idx(i)] + excess[_col_idx(i)] * latvap
+
+    worst[0] = 0.0
+    iw[0] = 0
+    if nptsexc[0] > 10:
+        for ii in range(1, nptsexc[0] + 1):
+            i = indxexc[_col_idx(ii)]
+            if excess[_col_idx(i)] < worst[0]:
+                worst[0] = excess[_col_idx(i)]
+                iw[0] = i
+
+    if trace_water_on != 0:
+        base_m = wtrc_iatype_vap[_col_idx(1)]
+        for ivap in range(1, iwtvap_n + 1):
+            m = wtrc_iatype_vap[_col_idx(ivap)]
+            for ii in range(1, nptsexc[0] + 1):
+                i = indxexc[_col_idx(ii)]
+                rat = _wtrc_ratio(
+                    iwspec[_col_idx(m)],
+                    qfxo[_q_idx(i, m, pcols)],
+                    qfxo[_q_idx(i, base_m, pcols)],
+                    wtrc_qmin,
+                    rstd,
+                )
+                qflx[_q_idx(i, m, pcols)] = qflx[_q_idx(i, m, pcols)] - rat * excess[_col_idx(i)]
