398288093a3419104e391166d868dba275b97894
Ruitong <cnitlrt@gmail.com>
2026-07-16 12:51:07 -0600

    Flatten RRTMG longwave standalone interface

--- src/atmos_phys/schemes/radiation/rrtmg_longwave/rad_rrtmg_lw.F90
+++ src/atmos_phys/schemes/radiation/rrtmg_longwave/rad_rrtmg_lw.F90
@@ -6,16 +6,8 @@ module ap_rad_rrtmg_lw_scheme
 !
 !-----------------------------------------------------------------------
 use shr_kind_mod,      only: r8 => shr_kind_r8
-use ppgrid,            only: pcols, pver, pverp
-use scamMod,           only: single_column, scm_crm_mode
 use parrrtm,           only: nbndlw, ngptlw
-use rrtmg_lw_init,     only: rrtmg_lw_ini
 use rrtmg_lw_rad,      only: rrtmg_lw
-use spmd_utils,        only: masterproc
-use perf_mod,          only: t_startf, t_stopf
-use cam_logfile,       only: iulog
-use cam_abortutils,    only: endrun
-use radconstants,      only: nlwbands
 
 implicit none
 
@@ -25,11 +17,7 @@ save
 ! Public methods
 
 public ::&
-   radlw_init,   &! initialize constants
    rad_rrtmg_lw_run   ! driver for longwave radiation code
-
-! Private data
-integer :: ntoplw    ! top level to solve for longwave cooling
 
 !===============================================================================
 CONTAINS
@@ -37,31 +25,37 @@ CONTAINS
 
 !> \section arg_table_rad_rrtmg_lw_run Argument Table
 !! \htmlinclude rad_rrtmg_lw_run.html
-subroutine rad_rrtmg_lw_run(lchnk   ,ncol      ,rrtmg_levs,r_state,       &
+subroutine rad_rrtmg_lw_run(pcols, pver, pverp, lchnk, ncol, rrtmg_levs, ntoplw, cpair, &
+                        pmidmb, pintmb, tlay, tlev, h2ovmr, o3vmr, co2vmr, ch4vmr, &
+                        o2vmr, n2ovmr, cfc11vmr, cfc12vmr, cfc22vmr, ccl4vmr, &
                         pmid    ,aer_lw_abs,cld       ,tauc_lw,       &
                         qrl     ,qrlc      ,                          &
                         flns    ,flnt      ,flnsc     ,flntc  ,flwds, &
                         flut    ,flutc     ,fnl       ,fcnl   ,fldsc, &
-                        lu      ,ld        )
+                        lu      ,ld, ful, fsul, fdl, fsdl)
 
 !-----------------------------------------------------------------------
-   use cam_history,         only: outfld
    use mcica_subcol_gen_lw, only: mcica_subcol_lw
-   use physconst,           only: cpair
-   use rrtmg_state,         only: rrtmg_state_t
 
 !------------------------------Arguments--------------------------------
 !
 ! Input arguments
 !
+   integer, intent(in) :: pcols, pver, pverp
    integer, intent(in) :: lchnk                 ! chunk identifier
    integer, intent(in) :: ncol                  ! number of atmospheric columns
    integer, intent(in) :: rrtmg_levs            ! number of levels rad is applied
+   integer, intent(in) :: ntoplw                 ! top level to solve for longwave cooling
+   real(r8), intent(in) :: cpair                 ! dry-air heat capacity
 
 !
 ! Input arguments which are only passed to other routines
 !
-    type(rrtmg_state_t), intent(in) :: r_state
+   real(r8), intent(in) :: pmidmb(:,:), pintmb(:,:)
+   real(r8), intent(in) :: tlay(:,:), tlev(:,:)
+   real(r8), intent(in) :: h2ovmr(:,:), o3vmr(:,:), co2vmr(:,:), ch4vmr(:,:)
+   real(r8), intent(in) :: o2vmr(:,:), n2ovmr(:,:), cfc11vmr(:,:), cfc12vmr(:,:)
+   real(r8), intent(in) :: cfc22vmr(:,:), ccl4vmr(:,:)
 
    real(r8), intent(in) :: pmid(:,:)             ! Level pressure (Pascals)
 
@@ -94,10 +88,10 @@ subroutine rad_rrtmg_lw_run(lchnk   ,ncol      ,rrtmg_levs,r_state,       &
 !
    integer :: i, k, kk, nbnd         ! indices
 
-   real(r8) :: ful(pcols,pverp)     ! Total upwards longwave flux
-   real(r8) :: fsul(pcols,pverp)    ! Clear sky upwards longwave flux
-   real(r8) :: fdl(pcols,pverp)     ! Total downwards longwave flux
-   real(r8) :: fsdl(pcols,pverp)    ! Clear sky downwards longwv flux
+   real(r8), intent(out) :: ful(pcols,pverp)     ! Total upwards longwave flux
+   real(r8), intent(out) :: fsul(pcols,pverp)    ! Clear sky upwards longwave flux
+   real(r8), intent(out) :: fdl(pcols,pverp)     ! Total downwards longwave flux
+   real(r8), intent(out) :: fsdl(pcols,pverp)    ! Clear sky downwards longwv flux
 
    integer :: inflglw               ! Flag for cloud parameterization method
    integer :: iceflglw              ! Flag for ice cloud param method
@@ -156,9 +150,6 @@ subroutine rad_rrtmg_lw_run(lchnk   ,ncol      ,rrtmg_levs,r_state,       &
    ! Call mcica sub-column generator for RRTMG_LW
 
    ! Call sub-column generator for McICA in radiation
-   call t_startf('ap_rad_rrtmg_lw_run')
-   call t_startf('mcica_subcol_lw')
-
    ! Select cloud overlap approach (1=random, 2=maximum-random, 3=maximum)
    icld = 2
    ! Set permute seed (must be offset between LW and SW by at least 140 to insure
@@ -174,11 +165,6 @@ subroutine rad_rrtmg_lw_run(lchnk   ,ncol      ,rrtmg_levs,r_state,       &
    call mcica_subcol_lw(lchnk, ncol, rrtmg_levs-1, icld, permuteseed, pmid(:, pverp-rrtmg_levs+1:pverp-1), &
       cld(:, pverp-rrtmg_levs+1:pverp-1), cicewp, cliqwp, rei, rel, tauc_lw(:, :ncol, pverp-rrtmg_levs+1:pverp-1), &
       cld_stolw, cicewp_stolw, cliqwp_stolw, rei_stolw, rel_stolw, tauc_stolw)
-
-   call t_stopf('mcica_subcol_lw')
-
-
-   call t_startf('rrtmg_lw')
 
    !
    ! Call RRTMG_LW model
@@ -215,16 +201,16 @@ subroutine rad_rrtmg_lw_run(lchnk   ,ncol      ,rrtmg_levs,r_state,       &
    ! Set aerosol optical depth to zero for now
 
    emis(:ncol,:nbndlw) = 1._r8
-   tsfc(:ncol) = r_state%tlev(:ncol,rrtmg_levs+1)
+   tsfc(:ncol) = tlev(:ncol,rrtmg_levs+1)
    taua_lw(:ncol, 1:rrtmg_levs-1, :nbndlw) = aer_lw_abs(:ncol,pverp-rrtmg_levs+1:pverp-1,:nbndlw)
 
    if (associated(lu)) lu(1:ncol,:,:) = 0.0_r8
    if (associated(ld)) ld(1:ncol,:,:) = 0.0_r8
 
    call rrtmg_lw(lchnk  ,ncol ,rrtmg_levs    ,icld    ,                 &
-        r_state%pmidmb  ,r_state%pintmb  ,r_state%tlay    ,r_state%tlev    ,tsfc    ,r_state%h2ovmr, &
-        r_state%o3vmr   ,r_state%co2vmr  ,r_state%ch4vmr  ,r_state%o2vmr   ,r_state%n2ovmr  ,r_state%cfc11vmr,r_state%cfc12vmr, &
-        r_state%cfc22vmr,r_state%ccl4vmr ,emis    ,inflglw ,iceflglw,liqflglw, &
+        pmidmb  ,pintmb  ,tlay    ,tlev    ,tsfc    ,h2ovmr, &
+        o3vmr   ,co2vmr  ,ch4vmr  ,o2vmr   ,n2ovmr  ,cfc11vmr,cfc12vmr, &
+        cfc22vmr,ccl4vmr ,emis    ,inflglw ,iceflglw,liqflglw, &
         cld_stolw,tauc_stolw,cicewp_stolw,cliqwp_stolw ,rei, rel, &
         taua_lw, &
         uflx    ,dflx    ,hr      ,uflxc   ,dflxc   ,hrc, &
@@ -260,13 +246,6 @@ subroutine rad_rrtmg_lw_run(lchnk   ,ncol      ,rrtmg_levs,r_state,       &
    fsul(:ncol,pverp-rrtmg_levs+1:pverp)=uflxc(:ncol,rrtmg_levs:1:-1)
    fsdl(:ncol,pverp-rrtmg_levs+1:pverp)=dflxc(:ncol,rrtmg_levs:1:-1)
 
-   if (single_column.and.scm_crm_mode) then
-      call outfld('FUL     ',ful,pcols,lchnk)
-      call outfld('FDL     ',fdl,pcols,lchnk)
-      call outfld('FULC    ',fsul,pcols,lchnk)
-      call outfld('FDLC    ',fsdl,pcols,lchnk)
-   endif
-
    fnl(:ncol,:) = ful(:ncol,:) - fdl(:ncol,:)
    ! mji/ cam excluded this?
    fcnl(:ncol,:) = fsul(:ncol,:) - fsdl(:ncol,:)
@@ -295,43 +274,6 @@ subroutine rad_rrtmg_lw_run(lchnk   ,ncol      ,rrtmg_levs,r_state,       &
            (/ncol,rrtmg_levs,nbndlw/), order=(/3,1,2/))
    end if
 
-   call t_stopf('rrtmg_lw')
-
-   call t_stopf('ap_rad_rrtmg_lw_run')
-
 end subroutine rad_rrtmg_lw_run
-
-!-------------------------------------------------------------------------------
-
-subroutine radlw_init()
-!-----------------------------------------------------------------------
-!
-! Purpose:
-! Initialize various constants for radiation scheme.
-!
-!-----------------------------------------------------------------------
-
-   use ref_pres, only : pref_mid
-
-   integer :: k
-
-   ! If the top model level is above ~90 km (0.1 Pa), set the top level to compute
-   ! longwave cooling to about 80 km (1 Pa)
-   if (pref_mid(1) .lt. 0.1_r8) then
-      do k = 1, pver
-         if (pref_mid(k) .lt. 1._r8) ntoplw  = k
-      end do
-   else
-      ntoplw  = 1
-   end if
-   if (masterproc) then
-      write(iulog,*) 'radlw_init: ntoplw =',ntoplw
-   endif
-
-   call rrtmg_lw_ini
-
-end subroutine radlw_init
-
-!-------------------------------------------------------------------------------
 
 end module ap_rad_rrtmg_lw_scheme
--- src/physics/rrtmg/radlw.F90
+++ src/physics/rrtmg/radlw.F90
@@ -3,13 +3,77 @@
 ! one scheme-module instance and are re-exported under the historical API.
 module radlw
 
-  use ap_rad_rrtmg_lw_scheme, only: radlw_init, &
-       rad_rrtmg_lw => rad_rrtmg_lw_run
+  use shr_kind_mod, only: r8 => shr_kind_r8
+  use ppgrid, only: pcols, pver, pverp
+  use rrtmg_state, only: rrtmg_state_t
+  use ap_rad_rrtmg_lw_scheme, only: rad_rrtmg_lw_run
 
   implicit none
   private
 
   public :: radlw_init
   public :: rad_rrtmg_lw
+
+  integer :: ntoplw
+
+contains
+
+  subroutine rad_rrtmg_lw(lchnk, ncol, rrtmg_levs, r_state, &
+       pmid, aer_lw_abs, cld, tauc_lw, qrl, qrlc, flns, flnt, &
+       flnsc, flntc, flwds, flut, flutc, fnl, fcnl, fldsc, lu, ld)
+
+    use cam_history, only: outfld
+    use perf_mod, only: t_startf, t_stopf
+    use physconst, only: cpair
+    use scamMod, only: single_column, scm_crm_mode
+
+    integer, intent(in) :: lchnk, ncol, rrtmg_levs
+    type(rrtmg_state_t), intent(in) :: r_state
+    real(r8), intent(in) :: pmid(:,:), aer_lw_abs(:,:,:), cld(:,:), tauc_lw(:,:,:)
+    real(r8), intent(out) :: qrl(:,:), qrlc(:,:), flns(:), flnt(:), flnsc(:), flntc(:)
+    real(r8), intent(out) :: flwds(:), flut(:), flutc(:), fnl(:,:), fcnl(:,:), fldsc(:)
+    real(r8), pointer, intent(inout) :: lu(:,:,:), ld(:,:,:)
+    real(r8) :: ful(pcols,pverp), fsul(pcols,pverp)
+    real(r8) :: fdl(pcols,pverp), fsdl(pcols,pverp)
+
+    call t_startf('ap_rad_rrtmg_lw_run')
+    call t_startf('mcica_subcol_lw')
+    call t_stopf('mcica_subcol_lw')
+    call t_startf('rrtmg_lw')
+    call rad_rrtmg_lw_run(pcols, pver, pverp, lchnk, ncol, rrtmg_levs, ntoplw, cpair, &
+         r_state%pmidmb, r_state%pintmb, r_state%tlay, r_state%tlev, &
+         r_state%h2ovmr, r_state%o3vmr, r_state%co2vmr, r_state%ch4vmr, &
+         r_state%o2vmr, r_state%n2ovmr, r_state%cfc11vmr, r_state%cfc12vmr, &
+         r_state%cfc22vmr, r_state%ccl4vmr, pmid, aer_lw_abs, cld, tauc_lw, &
+         qrl, qrlc, flns, flnt, flnsc, flntc, flwds, flut, flutc, fnl, fcnl, &
+         fldsc, lu, ld, ful, fsul, fdl, fsdl)
+    call t_stopf('rrtmg_lw')
+
+    if (single_column .and. scm_crm_mode) then
+       call outfld('FUL     ',ful,pcols,lchnk)
+       call outfld('FDL     ',fdl,pcols,lchnk)
+       call outfld('FULC    ',fsul,pcols,lchnk)
+       call outfld('FDLC    ',fsdl,pcols,lchnk)
+    end if
+    call t_stopf('ap_rad_rrtmg_lw_run')
+  end subroutine rad_rrtmg_lw
+
+  subroutine radlw_init()
+    use ref_pres, only: pref_mid
+    use spmd_utils, only: masterproc
+    use cam_logfile, only: iulog
+    use rrtmg_lw_init, only: rrtmg_lw_ini
+    integer :: k
+
+    if (pref_mid(1) < 0.1_r8) then
+       do k = 1, pver
+          if (pref_mid(k) < 1._r8) ntoplw = k
+       end do
+    else
+       ntoplw = 1
+    end if
+    if (masterproc) write(iulog,*) 'radlw_init: ntoplw =',ntoplw
+    call rrtmg_lw_ini
+  end subroutine radlw_init
 
 end module radlw
